497721e579bb5e445f18ed049b4ee894eec0ba89
Kara Marie Rawson <rawsonkara@gmail.com>
2017-10-09 23:53:37 -0400

    added images to demonstrate how to load assets in css and js

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,12 +2,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
+import imgElectron from '../assets/images/electron.png';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
+          <img className={styles.imgElectron} src={imgElectron}/>
           <h2>Home</h2>
           <Link to="/counter">to Counter</Link>
         </div>
